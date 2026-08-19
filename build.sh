034ba611f3c8cdb83aef1ae68dac92cef696cd56
Felixosk <248546902+Felixosk@users.noreply.github.com>
2026-08-19 13:13:36 +0300

    MeetingBlitz 1.4: update

--- build.sh
+++ build.sh
@@ -40,8 +40,8 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>CFBundleIdentifier</key>      <string>$BUNDLE_ID</string>
     <key>CFBundleExecutable</key>      <string>$APP_NAME</string>
     <key>CFBundlePackageType</key>     <string>APPL</string>
-    <key>CFBundleShortVersionString</key> <string>1.3</string>
-    <key>CFBundleVersion</key>         <string>4</string>
+    <key>CFBundleShortVersionString</key> <string>1.4</string>
+    <key>CFBundleVersion</key>         <string>5</string>
     <key>LSMinimumSystemVersion</key>  <string>14.0</string>
     <key>CFBundleIconFile</key>        <string>AppIcon</string>
     <key>LSUIElement</key>             <true/>
